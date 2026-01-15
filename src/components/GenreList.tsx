977bacedd8847128946d9350c99a9147c774cf54
Akshay-aN <Akshay-aN>
2026-01-15 13:11:21 +0530

    Shipping Static Data

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -6,7 +6,6 @@ import {
   List,
   ListItem,
   Spinner,
-  Text,
 } from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
@@ -27,7 +26,7 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
         Genres
       </Heading>
 
-      <List.Root>
+      <List.Root listStyleType="none">
         {data.map((genre) => (
           <ListItem key={genre.id} paddingY="5px">
             <HStack>
